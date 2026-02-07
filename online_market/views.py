0915fe145380466b6e13f31c5bb87ed5f4839f66
ALTUM-02 <martin249jesus@gmail.com>
2026-02-07 14:52:59 +0300

    new change

--- online_market/views.py
+++ online_market/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import Product
+from django.contrib.auth import login, logout, authenticate
+from .forms import RegisterForm
 
 # Create your views here.
 def home(request):
@@ -9,3 +11,16 @@ def home(request):
 def product_detail(request, id):
     product = Product.objects.get(id=id)
     return render(request, 'online_market_detail.html', {'product': product})
+
+def register(request):
+    form = RegisterForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    return render(request, 'online_market/reister.html', {'form': form})
+
+
+                    
