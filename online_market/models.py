d5cabd459269cae92e530717aeaae874f2ca5cf2
ALTUM-02 <martin249jesus@gmail.com>
2026-02-12 17:19:03 +0300

    new change

--- online_market/models.py
+++ online_market/models.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
+from .models import Product
 from django.db import models
 from django.contrib.auth.models import User
-
+admin.site.admin_register(Product)
 # Create your models here.
 class Product(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
