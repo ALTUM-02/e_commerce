ab805bdcfb65600832f6bdf04dd1f2d4667ddb3c
ALTUM-02 <martin249jesus@gmail.com>
2026-02-13 13:33:30 +0300

    new change

--- online_market/urls.py
+++ online_market/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('product/<int:id>/', views.product_detail, name='product_detail'),
     path('add_to_cart/<int:id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.cart_view, name='cart'),
-    path('order_history/', views.order_history, name='order_history')
+    path('order_history/', views.order_history, name='order_history'),
     
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
