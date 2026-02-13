bb274b9a442ad445e00ffab894af556be6c2368e
ALTUM-02 <martin249jesus@gmail.com>
2026-02-13 20:53:52 +0300

    new change

--- online_market/admin.py
+++ online_market/admin.py
@@ -4,3 +4,7 @@ from online_market.models import Product
 
 # Register your models here.
 admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'create_at')
+    search_fields = ('name', 'description')
