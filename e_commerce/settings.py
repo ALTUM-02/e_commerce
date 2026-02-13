001462ab8acfb87359943f1357e79ed323a63d06
ALTUM-02 <martin249jesus@gmail.com>
2026-02-13 12:16:20 +0300

    new change

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -123,4 +123,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-#LOGIN_REDIRECT_URL = 'product'
+LOGIN_REDIRECT_URL = '/cart/'
