de8beca522bc1def1b260ab4275ce8ada51ea5af
ALTUM-02 <martin249jesus@gmail.com>
2026-02-14 08:58:45 +0300

    new change

--- online_market/views.py
+++ online_market/views.py
@@ -69,6 +69,12 @@ def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            
+            if
         #Form = LoginForm(request.POST)
         i#f form.is_valid():
           #  user = form.save()
