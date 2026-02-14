e87c8f52c1ac7aba612f82dc5c8fd0b2b500aadb
ALTUM-02 <martin249jesus@gmail.com>
2026-02-14 22:11:24 +0300

    new change

--- online_market/views.py
+++ online_market/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Product,Cart, CartItem,Order
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate,  login as auth_login, logout
 from .forms import RegisterForm,LoginForm,ProductForm
 from django.contrib.auth.decorators import login_required
 
@@ -72,7 +72,7 @@ def login_view(request):
         
         user = authenticate(request, username=username, password=password)
         if user:
-            login(request, user)
+            auth_login(request, user)
             
             if user.is_superuser:
                 return redirect('admin_dashboard')
