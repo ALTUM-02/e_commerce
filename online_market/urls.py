1dc8ce4e45dbeedc460568c3682b650b5338fd01
ALTUM-02 <martin249jesus@gmail.com>
2026-02-14 21:38:51 +0300

    new change

--- online_market/urls.py
+++ online_market/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
+    path('dashboard/', views.user_dashboard, name='user_dashboard'),
+    
     path('logout/', views.logout, name='logout'),
     
 ]
