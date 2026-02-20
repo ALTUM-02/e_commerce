43f510603451cefc6ba405d6012c86fe72f7523c
ALTUM-02 <martin249jesus@gmail.com>
2026-02-20 12:30:11 +0300

    new addition

--- online_market/views.py
+++ online_market/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from django.http import JsonResponse
 from .models import Product,Cart, CartItem,Order
 from django.contrib.auth import authenticate,  login as auth_login, logout
@@ -53,6 +53,8 @@ def add_to_cart(request, id):
     
     return redirect('cart')  
 
+def add_to_cart_ajax(request):
+    if request.method == "POST":
 
 def cart_view(request):
     cart, created = Cart.objects.get_or_create(user=request.user)  
